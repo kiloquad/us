a5f59f90b0c1bfeca711445137fc54540ee7c121
Justin Zaun <justin.zaun@gmail.com>
2017-09-11 18:50:02 -1000

    Add Alabama city population infor for 2000 & 2010 Update city schema

--- examples/node/cities.js
+++ examples/node/cities.js
@@ -8,25 +8,25 @@ var arkansas = require('../../cities/arkansas.json');
 
 var data = _.concat(alabama, alaska, arizona, arkansas);
 
-var commonCityName = _.chain(data).groupBy('city').mapValues(function (p) {
+var commonCityName = _.chain(data).groupBy('name').mapValues(function (p) {
   return p.length;
 }).pickBy(function (d) {
   return d > 1;
 }).value();
 
 var commonCityEndings = {
-  ' City': _.filter(data, function (c) { return _.endsWith(c.city, ' City')}).length,
-  '-ville': _.filter(data, function (c) { return _.endsWith(c.city, 'ville')}).length,
-  '-dale': _.filter(data, function (c) { return _.endsWith(c.city, 'dale')}).length,
-  ' Valley': _.filter(data, function (c) { return _.endsWith(c.city, ' Valley')}).length,
-  ' Park': _.filter(data, function (c) { return _.endsWith(c.city, ' Park')}).length,
-  ' Point': _.filter(data, function (c) { return _.endsWith(c.city, ' Point')}).length,
-  '-burg': _.filter(data, function (c) { return _.endsWith(c.city, 'burg')}).length,
-  '-burgh': _.filter(data, function (c) { return _.endsWith(c.city, 'burgh')}).length,
-  '-boro': _.filter(data, function (c) { return _.endsWith(c.city, 'boro')}).length,
-  ' Ridge': _.filter(data, function (c) { return _.endsWith(c.city, ' Ridge')}).length,
-  ' Springs': _.filter(data, function (c) { return _.endsWith(c.city, ' Springs')}).length,
-  ' Village': _.filter(data, function (c) { return _.endsWith(c.city, ' Village')}).length
+  ' City': _.filter(data, function (c) { return _.endsWith(c.name, ' City')}).length,
+  '-ville': _.filter(data, function (c) { return _.endsWith(c.name, 'ville')}).length,
+  '-dale': _.filter(data, function (c) { return _.endsWith(c.name, 'dale')}).length,
+  ' Valley': _.filter(data, function (c) { return _.endsWith(c.name, ' Valley')}).length,
+  ' Park': _.filter(data, function (c) { return _.endsWith(c.name, ' Park')}).length,
+  ' Point': _.filter(data, function (c) { return _.endsWith(c.name, ' Point')}).length,
+  '-burg': _.filter(data, function (c) { return _.endsWith(c.name, 'burg')}).length,
+  '-burgh': _.filter(data, function (c) { return _.endsWith(c.name, 'burgh')}).length,
+  '-boro': _.filter(data, function (c) { return _.endsWith(c.name, 'boro')}).length,
+  ' Ridge': _.filter(data, function (c) { return _.endsWith(c.name, ' Ridge')}).length,
+  ' Springs': _.filter(data, function (c) { return _.endsWith(c.name, ' Springs')}).length,
+  ' Village': _.filter(data, function (c) { return _.endsWith(c.name, ' Village')}).length
 };
 
 console.log('\n\nCommon city names\n-----------------')
